79b1af84ace3d9f591222013ff6e7b804b0e0a27
Bhimesh Ganji <bhimeshganji5@gmail.com>
2025-08-26 14:03:19 +0530

    foodCatalouge service

--- foodOrdering/src/main/java/com/resto/foodOrdering/controller/FoodCatalougeController.java
+++ foodOrdering/src/main/java/com/resto/foodOrdering/controller/FoodCatalougeController.java
@@ -1,7 +1,14 @@
 package com.resto.foodOrdering.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.resto.foodOrdering.dto.FoodItemDto;
+import com.resto.foodOrdering.service.FoodCatalougeService;
 
 import lombok.AllArgsConstructor;
 
@@ -10,6 +17,15 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class FoodCatalougeController {
 	
+	private FoodCatalougeService foodCatalougeService;
+	
+	@PostMapping("/addFoodItem")
+	public ResponseEntity<FoodItemDto> addFoodItem(@RequestBody FoodItemDto foodItemDto)
+	{
+		FoodItemDto fooDto = foodCatalougeService.addFoodItem(foodItemDto);
+		return new ResponseEntity<>(fooDto,HttpStatus.CREATED);
+		
+	}
 	
 
 }
--- foodOrdering/src/main/java/com/resto/foodOrdering/service/FoodCatalougeService.java
+++ foodOrdering/src/main/java/com/resto/foodOrdering/service/FoodCatalougeService.java
@@ -2,6 +2,9 @@ package com.resto.foodOrdering.service;
 
 import org.springframework.stereotype.Service;
 
+import com.resto.foodOrdering.Entity.FoodItem;
+import com.resto.foodOrdering.dto.FoodItemDto;
+import com.resto.foodOrdering.mapper.FoodItemMapper;
 import com.resto.foodOrdering.repo.FoodCatalougeRepository;
 import com.resto.foodOrdering.service.impl.IFoodCatalougeService;
 
@@ -11,5 +14,10 @@ import lombok.AllArgsConstructor;
 public class FoodCatalougeService implements IFoodCatalougeService {
 
 	private FoodCatalougeRepository foodCatalougeRepository;
+
+	public FoodItemDto addFoodItem(FoodItemDto foodItemDto) {
+	FoodItem foodItemSavedInDB = foodCatalougeRepository.save(FoodItemMapper.toEntity(foodItemDto));
+	return FoodItemMapper.toDto(foodItemSavedInDB);
+	}
 	
 }
